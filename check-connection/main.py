9b64c6b99a8123e49740031dd9dc7653995ad9e8
pubudusanka <pubudusanka79@gmail.com>
2024-10-15 22:20:03 +0530

    add connection check code to main.py

--- check-connection/main.py
+++ check-connection/main.py
@@ -16,3 +16,29 @@ cTime = 0
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
+
+try:
+    arduino = serial.Serial(port='COM5', baudrate=9600, timeout=1, write_timeout=2)
+    time.sleep(2)  # Wait for the serial connection to initialize
+    arduino_connected = True
+
+    # Send data to Arduino
+    arduino.write(b'1')  # Send '1' to the Arduino
+    print("=========================================")
+    print("Sent request to arduino")
+    # Wait a moment for the Arduino to respond
+    time.sleep(1)
+
+    # Read response from Arduino
+    response = arduino.readline().decode('utf-8').strip()  # Read and decode response
+    if response:
+        print("Arduino response to the request")
+    else:
+        print("No response from Arduino")
+
+except serial.serialutil.SerialException as e:
+    print(f"Error Connecting to Arduino: {e}")
+    arduino_connected = False
+
+print("Arduino connection is : ", arduino_connected)
+print("=========================================")
